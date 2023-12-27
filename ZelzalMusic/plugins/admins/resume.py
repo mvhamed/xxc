d1af82c639e1e0d5e7c7e631b6e1328cb207326b
Zilzalll <146859836+Zilzalll@users.noreply.github.com>
2023-12-27 23:35:50 +0300

    Update resume.py

--- ZelzalMusic/plugins/admins/resume.py
+++ ZelzalMusic/plugins/admins/resume.py
@@ -14,7 +14,7 @@ from ZelzalMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(command(["كمل", "كملي"]) & filters.group & ~BANNED_USERS)
+@app.on_message(command(["كمل", "كملي"]) & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
